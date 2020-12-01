7dbe7c88abc8f5862c1a92eb94898d9ac4510860
Ben Lostheart <68131793+Ben-Lost@users.noreply.github.com>
2020-11-30 17:47:56 -0800

    Update UI.cpp

--- UI.cpp
+++ UI.cpp
@@ -10,6 +10,8 @@ int movement(cell map[128][128], hero &p, int input)
     {
         //Move left
         case 1:
+            if(p.x_pos == 0)
+                return 0;
             dest = map[p.y_pos][p.x_pos - 1];
             //Returns on failure if there's a wall and consumes energy.
             if(dest.tile == 'w' || dest.tile == 'b'){
@@ -30,6 +32,8 @@ int movement(cell map[128][128], hero &p, int input)
 
         //Move Up
         case 2:
+            if(p.y_pos == 0)
+                return 0;
             dest = map[p.y_pos - 1][p.x_pos];
             //Returns on failure if there's a wall and consumes energy.
             if(dest.tile == 'w' || dest.tile == 'b'){
@@ -50,6 +54,8 @@ int movement(cell map[128][128], hero &p, int input)
 
         //Move Right
         case 3:
+            if(p.x_pos == COLS)
+                return 0;
             dest = map[p.y_pos][p.x_pos + 1];
 
             //Returns on failure if there's a wall and consumes energy.
@@ -71,6 +77,8 @@ int movement(cell map[128][128], hero &p, int input)
 
         //Move down
         case 4:
+            if(p.y_pos == LINES)
+                return 0;
             dest = map[p.y_pos + 1][p.x_pos];
 
             //Returns on failure if there's a wall and consumes energy.
@@ -88,6 +96,19 @@ int movement(cell map[128][128], hero &p, int input)
 
             //update player location to (y+1,x)
             ++p.y_pos;
+            break;
+
+        case 5:
+            if(map[p.y_pos][p.x_pos].toolDevice) {
+              p.purchase_tool(map[p.y_pos][p.x_pos].toolDevice);
+              map[p.y_pos][p.x_pos].toolDevice = NULL;
+              map[p.y_pos][p.x_pos].symbol = '/';
+            }   
+            else if(map[p.y_pos][p.x_pos].foodUnit) {
+              p.purchase_food(map[p.y_pos][p.x_pos].foodUnit);
+              map[p.y_pos][p.x_pos].foodUnit = NULL;
+              map[p.y_pos][p.x_pos].symbol = '/';
+            }   
             break;
 
     } 
