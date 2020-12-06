c0adb7b1f997465a1ce37e6d1c6eb8e8f676d2de
Ben Lostheart <68131793+Ben-Lost@users.noreply.github.com>
2020-12-06 00:36:21 -0800

    Update library.h

--- library.h
+++ library.h
@@ -3,6 +3,8 @@
 #include <ncurses.h>
 #include <fstream>
 
+#define BINOCULARS 100
+
 struct obstacle{
   char name[100];
   char tool[100];
@@ -16,9 +18,11 @@ struct food{
 };
 
 struct tool{
+
+  tool();
   tool(const tool & source);
-  void display(WINDOW * win);
-  
+  void display(WINDOW * win);       
+
   char name[100];
   int power;
   int cost;
@@ -31,10 +35,41 @@ struct treasure{
   tool loot2[100];
 };
 
+class inventory
+{
+  public:
+    inventory();
+    ~inventory();
+  
+    int add_tool(tool & copy_from);
+    //int remove_tool(char * to_delete);
+    tool * retrieve_tool(int num);
+    void display(WINDOW * win);
+    void remove(int);
+
+  protected:
+    void initialize(tool ** &ptr, int size);
+    int add_tool(tool ** &ptr, tool & copy_from);
+    //int remove_tool(tool ** &inventory, char * to_delete);
+    void delete_all(tool **& ptr);
+    void display(WINDOW * win, tool ** ptr, int y, int count);
+
+    tool ** backpack;
+    int size;
+};
+
 // player
 struct hero{
+  int purchase_tool(tool * a_tool);
+  int purchase_food(food * a_food);
+  int purchase_ship();
+  void use_tool(tool *, obstacle *);
+
+  bool has_ship;
   int energy;
-  int cash;
+  int whiffle;
+  int sight;
+  int clue_counter;
   int x_pos;
   int y_pos;
   inventory backpack;
@@ -58,4 +93,6 @@ struct cell{
   food* foodUnit;
   tool* toolDevice;
   treasure* treasureChest;
+  char* clue;
+  bool curs;
 };
