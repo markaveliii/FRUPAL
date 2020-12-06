c965f19d679d8ae2d429fafb7fa3f1e7a2f8fe80
Ben Lostheart <68131793+Ben-Lost@users.noreply.github.com>
2020-12-06 00:37:51 -0800

    Update UI.cpp

--- UI.cpp
+++ UI.cpp
@@ -1,110 +1,202 @@
 #include "UI.h"
 
-int movement(cell map[128][128], hero &p)
+int movement(cell map[128][128], hero &p, int input)
 {
-    noecho();
-    keypad(stdscr, TRUE);
-    nodelay(stdscr, TRUE);
-    cell dest;
+  if(p.energy <= 0)
+    return -1;
+  noecho();
+  keypad(stdscr, TRUE);
+  nodelay(stdscr, FALSE);
+  cell dest;
+  switch(input)
+  {
+    //Move left
+    case 'a':
+    case 'A':
 
-    switch(getch())
-    {
-        //Move left
-        case 1:
-            if(p.x_pos == 0)
-                return 0;
-            dest = map[p.y_pos][p.x_pos - 1];
-            //Returns on failure if there's a wall and consumes energy.
-            if(dest.tile == 'w' || dest.tile == 'b'){
-                --p.energy;
-                return 0;
-            }
-            //Handles obstacles
-            if(dest.symbol == '!'){
-                p.energy -= dest.obsType->drain;
-            }
-            else{
-            p.energy -= dest.drain;
-            }
+      if(p.x_pos == 0)
+        return 0;
+      dest = map[p.y_pos][p.x_pos - 1];
+      //handles if player exits ship
+      if(p.has_ship == true && dest.tile != 'b') {
+        p.has_ship = false;
+        map[p.y_pos][p.x_pos].symbol = 'S';
+      }
+      //Returns on failure if there's a wall and consumes energy.
+      if(dest.tile == 'w'){
+          --p.energy;
+          return 0;
+      }
+      //handles if theres water and if player has ship or not
+      if(dest.tile == 'b' && dest.symbol != 'S') {
+          if(!p.has_ship) {
+            --p.energy;
+            return 0;
+          }
+      }
+      //Handles obstacles
+      if(dest.symbol == '!'){
+        p.energy -= dest.obsType->drain;
+        map[p.y_pos][p.x_pos-1].symbol = '/';
+      }
+      else{
+        p.energy -= dest.drain;
+      }
 
-            //update player location to (y,x-1)
-            --p.x_pos;
-            break;
+   
+      //update player location to (y,x-1)
+      --p.x_pos;
+      break;
 
-        //Move Up
-        case 2:
-            if(p.y_pos == 0)
-                return 0;
-            dest = map[p.y_pos - 1][p.x_pos];
-            //Returns on failure if there's a wall and consumes energy.
-            if(dest.tile == 'w' || dest.tile == 'b'){
-                --p.energy;
-                return 0;
-            }
-            //Handles obstacles
-            if(dest.symbol == '!'){
-                p.energy -= dest.obsType->drain;
-            }
-            else{
-            p.energy -= dest.drain;
-            }
+      //Move Up
+    case 'w':
+    case 'W':
+      if(p.y_pos == 0)
+        return 0;
+      dest = map[p.y_pos - 1][p.x_pos];
+        //handles if player exits ship
+      if(p.has_ship == true && dest.tile != 'b') {
+        p.has_ship = false;
+        map[p.y_pos][p.x_pos].symbol = 'S';
+      }
+      //Returns on failure if there's a wall and consumes energy.
+      if(dest.tile == 'w'){
+          --p.energy;
+          return 0;
+      }
+      //handles if theres water and if player has ship or not
+      if(dest.tile == 'b' && dest.symbol != 'S') {
+          if(!p.has_ship) {
+            --p.energy;
+            return 0;
+          }
+      }
+      //Handles obstacles
+      if(dest.symbol == '!'){
+        p.energy -= dest.obsType->drain;
+        map[p.y_pos-1][p.x_pos].symbol = '/';
+      }
+      else{
+        p.energy -= dest.drain;
+      }
 
-            //update player location to (y-1,x)
-            --p.y_pos;
-            break;
+      //update player location to (y-1,x)
+      --p.y_pos;
+      break;
 
-        //Move Right
-        case 3:
-            if(p.x_pos == COLS)
-                return 0;
-            dest = map[p.y_pos][p.x_pos + 1];
+      //Move Right
+    case 'd':
+    case 'D':
+      if(p.x_pos == COLS)
+        return 0;
+      dest = map[p.y_pos][p.x_pos + 1];
+       //handles if player exits ship
+      if(p.has_ship == true && dest.tile != 'b') {
+        p.has_ship = false;
+        map[p.y_pos][p.x_pos].symbol = 'S';
+      }
+      //Returns on failure if there's a wall and consumes energy.
+      if(dest.tile == 'w'){
+          --p.energy;
+          return 0;
+      }
+      //handles if theres water and if player has ship or not
+      if(dest.tile == 'b' && dest.symbol != 'S') {
+          if(!p.has_ship) {
+            --p.energy;
+            return 0;
+          }
+      }
+      //Handles obstacles
+      if(dest.symbol == '!'){
+        p.energy -= dest.obsType->drain;
+        map[p.y_pos][p.x_pos+1].symbol = '/';
+      }
+      else{
+        p.energy -= dest.drain;
+      }
 
-            //Returns on failure if there's a wall and consumes energy.
-            if(dest.tile == 'w' || dest.tile == 'b'){
-                --p.energy;
-                return 0;
-            }
-            //Handles obstacles
-            if(dest.symbol == '!'){
-                p.energy -= dest.obsType->drain;
-            }
-            else{
-            p.energy -= dest.drain;
-            }
+      //update player location to (y,x+1)
+      ++p.x_pos;
+      break;
 
-            //update player location to (y,x+1)
-            ++p.x_pos;
-            break;
+      //Move down
+    case 's':
+    case 'S':
+      if(p.y_pos == LINES)
+        return 0;
+      dest = map[p.y_pos + 1][p.x_pos];
+       //handles if player exits ship
+      if(p.has_ship == true && dest.tile != 'b') {
+        p.has_ship = false;
+        map[p.y_pos][p.x_pos].symbol = 'S';
+      }
+      //Returns on failure if there's a wall and consumes energy.
+      if(dest.tile == 'w'){
+          --p.energy;
+          return 0;
+      }
+      //handles if theres water and if player has ship or not
+      if(dest.tile == 'b' && dest.symbol != 'S') {
+          if(!p.has_ship) {
+            --p.energy;
+            return 0;
+          }
+      }
+      //Handles obstacles
+      if(dest.symbol == '!'){
+        p.energy -= dest.obsType->drain;
+        map[p.y_pos+1][p.x_pos].symbol = '/';
+      }
+      else{
+        p.energy -= dest.drain;
+      }
 
-        //Move down
-        case 4:
-            if(p.y_pos == LINES)
-                return 0;
-            dest = map[p.y_pos + 1][p.x_pos];
+      //update player location to (y+1,x)
+      ++p.y_pos;
+      break;
 
-            //Returns on failure if there's a wall and consumes energy.
-            if(dest.tile == 'w' || dest.tile == 'b'){
-                --p.energy;
-                return 0;
-            }
-            //Handles obstacles
-            if(dest.symbol == '!'){
-                p.energy -= dest.obsType->drain;
-            }
-            else{
-                p.energy -= dest.drain;
-            }
+    case 'r':
+    case 'R':
+      if(map[p.y_pos][p.x_pos].toolDevice) {
+        p.purchase_tool(map[p.y_pos][p.x_pos].toolDevice);
+        map[p.y_pos][p.x_pos].toolDevice = NULL;
+        map[p.y_pos][p.x_pos].symbol = '/';
+      }   
+      else if(map[p.y_pos][p.x_pos].foodUnit) {
+        p.purchase_food(map[p.y_pos][p.x_pos].foodUnit);
+        map[p.y_pos][p.x_pos].foodUnit = NULL;
+        map[p.y_pos][p.x_pos].symbol = '/';
+      }
+      else if(map[p.y_pos][p.x_pos].symbol == 'B'){
+        if(p.whiffle >= BINOCULARS){
+          p.whiffle -= BINOCULARS;
+          ++p.sight;
+          map[p.y_pos][p.x_pos].symbol = '/';
+          mvprintw(12, (COLS - COLS/4) + 3, "Purchase success");
+          mvprintw(13, (COLS - COLS/4) + 3, "Press any key to continute.");
+          getch();
+          move(12, (COLS - COLS/4) + 3);
+          clrtoeol();
+          move(13, (COLS - COLS/4) + 3);
+          clrtoeol();
+          refresh();
+        }
+      }
+   //Handle treasure
+      else if(map[p.y_pos][p.x_pos].symbol == '$') {
+        p.whiffle += map[p.y_pos][p.x_pos].treasureChest->whiffle;
+        map[p.y_pos][p.x_pos].symbol = '/';
+      }
 
-            //update player location to (y+1,x)
-            ++p.y_pos;
-            break;
-
-    } 
-    return 1;
+      break;
+  } 
+  return 1;
 }
 
-int purchase(cell &dest)
-{
-    int cost;
-    return cost;
-}
+/*int purchase(cell &dest)
+  {
+  int cost;
+  return cost;
+  }
+ */
